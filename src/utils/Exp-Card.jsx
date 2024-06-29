016e042bbf335422586976379007243a578094a8
Jainish Patel <jaicode2020@gmail.com>
2024-06-29 13:27:28 -0400

    added links for experience cards

--- src/utils/Exp-Card.jsx
+++ src/utils/Exp-Card.jsx
@@ -7,6 +7,7 @@ export const Experience = [
     Role: 'Co-Founder | CTO', 
     Date: '/ March 2024 - Present',
     Description: 'StatOracle is an upcoming football (soccer) statistic software & startup. Leading development of StatOracle in all domains including web development, AI model development, and building end-to-end pipelines.',
+    Link: ''
   },
   {
     image: './assets/opennlp-logo.jpeg',
@@ -16,6 +17,7 @@ export const Experience = [
     Role: 'Research Member', 
     Date: '/ June 2024 - Present',
     Description: "Conducting research on image transcreation to enhance cultural sensitivity, working with a dedicated team of high school students under the mentorship of a PhD candidate from Carnegie Mellon University. Preparing for an upcoming collaboration with Stanford's SALT Lab.",
+    Link: 'https://opennlplabs.org/'
   },
   {
     image: './assets/factful-logo.jpeg',
@@ -25,6 +27,7 @@ export const Experience = [
     Role: 'Backend & AI Developer', 
     Date: '/ April 2024 - Present',
     Description: "Backend & AI (particularly LLMs) developer at Factful, where I specialize in using web scraping techniques to gather crucial data for our AI-generated suggestions platform and find-tune LLMs. Also developed a custom RAG architecture to ensure suggestions are unbiased and can cover recent events.",
+    Link: 'https://factful.io/'
   },
   {
     image: './assets/reteena-logo.jpeg',
@@ -34,6 +37,7 @@ export const Experience = [
     Role: 'Co-Founder', 
     Date: '/ March 2024 - Present',
     Description: "Co-Founder of Reteena, a verified 503(c)(3) nonprofit under a fiscal sponsorship from Hack Club. We aim to reduce healthcare diagnosis costs by utilizing AI models. Currently working on using AI models to inference for Alzheimer's using low-resolution MRI scans and working on building a REST API to host the AI model diagnosis on the website.",
+    Link: 'https://www.reteena.org/'
   },
   {
     image: './assets/nop-logo.png',
@@ -43,6 +47,7 @@ export const Experience = [
     Role: 'Programming Lead', 
     Date: '/ August 2022 - Present',
     Description: "Programming Lead for FRC team 4065, Nerds of Prey. Led a 7+ students team to program our team's robot for the annual FRC challenge. This task involved teaching advanced concepts and fundamentals of Java along with the WPILib framework. Actively used pose estimation of robots, object position estimation using AI models, simulation, logging, mechanism control using algorithms such as PID, Feedforward, and motion profiling. Led the team to win our first Autonomous Award. Also participated in hosting numerous outreach events along with other students to promote STEM and mentored local robotics teams.",
+    Link: 'https://www.frc4065.com/'
   }
 ]
 
